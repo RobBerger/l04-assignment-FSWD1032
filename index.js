7609a916c60ef0a75b3226ac16f14148bf7e21f3
berger <robertberger.dev@gmail.com>
2023-01-24 11:22:39 -0500

    ran npm start

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 "use strict";
 function toString(logObj) {
-    console.log(`${logObj.firstName} ${logObj.lastName} is ${logObj.age} and his favorite color is ${logObj.favoriteColor}`);
+    console.log(`${logObj.firstName} ${logObj.lastName} is ${logObj.age} and his favorite color is ${logObj.favoriteColor}.`);
+}
+function method_printContactDetails(printObj) {
+    console.log(`${printObj.firstName} ${printObj.lastName} is ${printObj.age} years old and her favorite color is ${printObj.favoriteColor}.\n${printObj.firstName} can be reached at ${printObj.phoneNumber} or ${printObj.email}.`);
 }
 let joe = {
     firstName: "Joe",
@@ -8,4 +11,13 @@ let joe = {
     age: 30,
     favoriteColor: "Blue"
 };
+let taylor = {
+    firstName: "Taylor",
+    lastName: "Maylor",
+    age: 32,
+    favoriteColor: "Magenta",
+    phoneNumber: "123-456-7890",
+    email: "taylor.maylor@test.com"
+};
 toString(joe);
+method_printContactDetails(taylor);
